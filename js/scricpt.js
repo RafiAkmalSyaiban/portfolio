89c308568c9982d79924c9fff78ce1e5ba4c7e26
RafiAkmalSyaiban <syaibanrafiakmal@gmail.com>
2024-06-15 08:34:54 +0700

    first init github

---
+++ js/scricpt.js
@@ -0,0 +1,8 @@
+/*======================== typing animasi======================*/
+var typed = new Typed(".typing",{
+    strings:["","islam nusantara university"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+
+})
